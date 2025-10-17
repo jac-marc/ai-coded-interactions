7e410b5e1b52e25dac4a69f009601b11060886ae
jac-marc <jacopo.marcolini@gmail.com>
2025-10-17 15:07:31 +0200

    tolte delle pagine

--- app/navigation.js
+++ app/navigation.js
@@ -7,18 +7,11 @@ const Navbar = () => {
             <Link className="pr-10" href="/page-loader">Page loader</Link>
             <Link className="pr-10" href="/infinite">Infinite</Link>
             <Link className="pr-10" href="/image">Moving image</Link>
-<<<<<<< HEAD
-=======
-            <Link className="pr-10" href="/masonry-grid">Masonry</Link>
->>>>>>> b2a10674 (prova)
             <Link className="pr-10" href="/var-font">Variable font</Link>
             <Link className="pr-10" href="/var-font-2">Variable font 2</Link>
             <Link className="pr-10" href="/blur-font">Blur</Link>
             <Link className="pr-10" href="/about">About</Link>
-<<<<<<< HEAD
-=======
             <Link className="pr-10" href="/tracker">Tracker</Link>
->>>>>>> b2a10674 (prova)
         </nav>
     );
 }
